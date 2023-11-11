13de99f42f8ed0397da07fc577e973dc9924aac3
npetroselli <nicopetroselli@gmail.com>
2023-11-11 00:08:41 -0300

    Segundo commit. Primer preentrega funcionando

--- main.js
+++ main.js
@@ -1,46 +1,15 @@
-//***********IDEA 1: Calculadora********** */
-/* El factorial de un entero positivo n, el factorial de n o n factorial se define en principio como el producto de todos los números enteros positivos desde 1 (es decir, los números naturales) hasta n. Por ejemplo:
+function solicitarNumero() {
+  let numero = parseInt(prompt("Ingresa un número entero: "));
+  let factorial = 1;
 
-5! = 1 x 2 x 3 x 4 x 5 = 120.
- 
-5! = 1  \times  2  \times  3  \times  4 \times  5= 120.
-
-Para este ejemplo vamos a limitar las posibilidades de ingreso desde el 1 al 10, para repasar condicionales y operadores lógicos*/
-
-//**********IDEA 2: Pago de producto en cuotas + interés, y con descuento en efectivo*********** */
-/* Calcular pagos en cuotas sobre un monto determinado.
-El precio final del producto se calculará según los medios de pago:
-Efectivo: 10% descuento
-Débito; o
-Crédito: el interés a percibir depende de las cuotas a elegir (3, 6 o 12).  */
-
-/* function cuotas(monto, cuotas, tasaInteres) {
-    const interesMensual = (monto * (tasaInteres / 100)) / 12;
-    const cuotaMensual = (monto + interesMensual) / cuotas;
-    return cuotaMensual;
+  for (let i = 1; i <= numero; i = i + 1) {
+    factorial = factorial * i;
+  }
+  if (numero < 0) {
+    alert("Sólo números enteros positivos.");
+  } else {
+    alert("El factorial de " + numero + " es " + factorial);
+  }
 }
 
-let resultado = cuotas(20000, 3, (monto * 0.10));
-console.log(resultado);
- */
-
-prompt("Ingrese "), prompt(segundoNumero), prompt(operacion)
-function calculadora(primerNumero, segundoNumero, operacion) {
-    switch (operacion) {
-        case "+":
-            return primerNumero + segundoNumero;
-            break;
-        case "-":
-            return primerNumero - segundoNumero;
-            break;
-        case "*":
-            return primerNumero * segundoNumero;
-            break;
-        case "/":
-            return primerNumero / segundoNumero;
-            break;
-        default:
-            return 0;
-            break;
-    }
-}
+solicitarNumero();
